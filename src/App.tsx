d391d6243e83c4347240d3040676b26466f5e875
AJ1732 <ejemeniboi@gmail.com>
2024-05-05 15:33:10 +0100

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+function App() {
+
+  return (
+    <div className="content-grid all | min-h-dvh">
+      {/* NAVBAR */}
+      <nav className="full-width content-grid |">
+        <div>
+          NavBar
+        </div>
+      </nav>
+
+      {/* MAIN CONTENT */}
+      <main className="content">
+        <h1>Hello</h1>
+      </main>
+    </div>
+  )
+}
+
+export default App
