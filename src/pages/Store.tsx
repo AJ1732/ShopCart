1595d3bd7c85eb7e848faad016f3576013cc7629
AJ1732 <ejemeniboi@gmail.com>
2024-05-05 17:31:00 +0100

    feat: center grid

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -7,11 +7,11 @@ const Store = () => {
         <h2 className='text-3xl font-semibold'>Store</h2>
       </header>
 
-      <section className='w-4/5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4'>
+      <section className='w-4/5 mx-auto sm:w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4'>
         {storeItems.map(({ id }) => (
           <div 
             key={id}
-            className='w-full bg-white bg-opacity-55 py-1 px-3 border border-[#1a1a1a] rounded-md'  
+            className='w-full min-h-8 bg-white bg-opacity-55 py-1 px-3 border border-[#1a1a1a] rounded-md'  
           >
             Item
           
