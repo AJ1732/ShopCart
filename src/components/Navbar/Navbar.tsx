76cda92690c61b3b6d072e77aaf8536cfc94811a
AJ1732 <ejemeniboi@gmail.com>
2024-05-05 16:54:30 +0100

    feat: clean styles and add cart icon

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,26 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
+import cart from  '../../assets/cart.svg'
 
 const Navbar = () => {
   return (
-    <nav className="full-width content-grid | bg-[#353535] text-white drop-shadow-2xl">
+    <nav className="full-width content-grid | bg-[#1a1a1a] text-white drop-shadow-2xl">
       <div className="flex justify-between items-center">
         <header>
           <h1 className="text-[clamp(1.5rem,_2vw_+_1rem,_2rem)]">Logo</h1>
         </header>
         
-        <ul className="text-xl | flex basis-40 justify-between items-center gap-2">
-          <Link to={`/`}>Home</Link>
-          <Link to={`/store`}>Store</Link>
-          <Link to={`/about`}>About</Link>
+        <ul className="font-semibold text-xl | flex basis-40 justify-between items-center gap-2">
+          <NavLink to={`/`}>Home</NavLink>
+          <NavLink to={`/store`}>Store</NavLink>
+          <NavLink to={`/about`}>About</NavLink>
         </ul>
       </div>
+
+      <button className="fixed top-20 end-1">
+        <figure className="size-10">
+          <img src={cart} alt="" />
+        </figure>
+      </button>
     </nav>
   )
 }
