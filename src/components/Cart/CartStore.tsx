ce5049d6083b54b79e6cfdaa00df60c7b3035ad2
AJ1732 <ejemeniboi@gmail.com>
2024-05-06 17:32:39 +0100

    feat: create cart item

--- src/components/Cart/CartStore.tsx
+++ src/components/Cart/CartStore.tsx
@@ -1,20 +1,19 @@
 import { createPortal } from "react-dom"
 import { useStoreContext } from "../../context/StoreProvider"
+import CartItem from "./CartItem";
 
 const CartStore = () => {
-  const { open, toggleCart } = useStoreContext();
-
-  // if (!open) {
-  //   return null
-  // }
+  const { open, toggleCart, cartItems } = useStoreContext();
 
   return createPortal(
     <section className={`absolute z-[2] top-0 ${open? "right-0": "-right-full"} transition-all duration-500
      w-80 h-full bg-[#EAA93100] bg-[#ccc] pt-24 p-4 shadow-left`}>
       <button onClick={toggleCart} className="absolute right-5 text-white hover:bg-black"> X </button>
 
-      <div>
-        CartStore
+      <div className="mt-16">
+        {cartItems.map( item => (
+          <CartItem key={item.id} {...item} />
+        ))}
       </div>
     </section>,
     document.getElementById("portal")!
