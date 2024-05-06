e5a86c50371cea16910dd30b961ea9c2d8876d52
AJ1732 <ejemeniboi@gmail.com>
2024-05-06 20:05:32 +0100

    feat: add total price to store

--- src/components/Cart/CartStore.tsx
+++ src/components/Cart/CartStore.tsx
@@ -1,7 +1,8 @@
 import { createPortal } from "react-dom"
 import { useStoreContext } from "../../context/StoreProvider"
 import CartItem from "./CartItem";
-
+import { formatCurrency } from "../../utils/formatCurrency";
+import storeItems from "../../data/items.json";
 const CartStore = () => {
   const { open, toggleCart, cartItems } = useStoreContext();
 
@@ -9,6 +10,7 @@ const CartStore = () => {
     <section className={`
       absolute z-[2] top-0 ${open? "right-0": "-right-full"} transition-all duration-500
       w-96 sm:w-[500px] h-full bg-[#EAA93100] bg-[#ccc] pt-24 p-3 shadow-left  
+      flex flex-col justify-between
     `}>
       <button onClick={toggleCart} className="absolute right-5 text-white hover:bg-black">Close</button>
 
@@ -16,6 +18,18 @@ const CartStore = () => {
         {cartItems.map( item => (
           <CartItem key={item.id} {...item} />
         ))}
+      </div>
+
+      <div className="text-2xl mt-4">
+        Total:
+        <span className="font-bold">
+          {
+            formatCurrency(cartItems.reduce((total, cartItem) => {
+              const item = storeItems.find(i => i.id === cartItem.id )
+              return total + (item?.price || 0) * cartItem.quantity
+            }, 0))
+          }
+        </span>
       </div>
     </section>,
     document.getElementById("portal")!
